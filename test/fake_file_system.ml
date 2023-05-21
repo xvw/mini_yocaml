a2523aa694882a9dc2064189e01cd30c96264400
xvw <xavier.vdw@gmail.com>
2023-05-21 16:32:52 +0200

    Fix `update_elt`

--- test/fake_file_system.ml
+++ test/fake_file_system.ml
@@ -81,6 +81,56 @@ let get_elt fs path_str =
   aux fs path
 ;;
 
+let extract_target path =
+  let rec aux acc = function
+    | [ x ] -> Some (List.rev acc, x)
+    | x :: xs -> aux (x :: acc) xs
+    | [] -> None
+  in
+  aux [] path
+;;
+
+let update_elt fs path_str f =
+  let path = String.split_on_char '/' path_str in
+  match extract_target path with
+  | None -> fs
+  | Some (path, target) ->
+    let rec aux acc fs path =
+      match fs, path with
+      | [], [] ->
+        (* We are in the right directory and the
+           file does not exist, we must create it. *)
+        f target None :: acc
+      | (File { name; mtime; content } as current_file) :: fs_xs, [] ->
+        (* We are in the right directory  *)
+        if String.equal name target
+        then (
+          (* This is the file that needs to be modified *)
+          let new_file = f target (Some (mtime, content)) in
+          new_file :: (acc @ fs_xs))
+        else (* This is not the right file *)
+          aux (current_file :: acc) fs_xs []
+      | (Dir { name; children; _ } as current_dir) :: fs_xs, fragment :: path_xs
+        ->
+        (* We enter a folder *)
+        if String.equal name fragment
+        then (
+          (* You must enter the directory *)
+          let new_dir = dir name (aux [] children path_xs) in
+          new_dir :: (acc @ fs_xs))
+        else (* This is not the right dir *)
+          aux (current_dir :: acc) fs_xs path
+      | [], fragment :: path_xs ->
+        (* You must create a new directory *)
+        let new_dir = dir fragment (aux [] [] path_xs) in
+        new_dir :: acc
+      | x :: fs_xs, path ->
+        (* It is necessary to continue to go through the tree *)
+        aux (x :: acc) fs_xs path
+    in
+    aux [] fs path
+;;
+
 let log _ = ()
 let fail message = Error message
 let file_exists fs path = Option.is_some (get_elt fs path)
@@ -97,10 +147,16 @@ let read_file fs path =
   | _ -> Error ("file" ^ path ^ " does not exists")
 ;;
 
+let write_file ?at fs path content =
+  update_elt fs path (fun target _ -> file ?mtime:at target content)
+;;
+
 (* Test *)
 
 (* Since this module is present only for
    testing purpose, we can inline tests *)
+
+let pipe_write_file ?at path content fs = write_file fs ?at path content
 
 let test_get_elt =
   let open Alcotest in
@@ -156,4 +212,120 @@ let test_get_elt =
       (get_elt [ fs ] "./post"))
 ;;
 
-let test_cases = "Fake_file_system", [ test_get_elt ]
+let test_update_elt_1 =
+  let open Alcotest in
+  test_case "Some test using update_elt" `Quick (fun () ->
+    let fs =
+      [ dir
+          "."
+          [ file "hello.txt" "Hello, World!"
+          ; dir
+              "post"
+              [ file "index.md" "First post"
+              ; file "second.md" "second post"
+              ; dir "images" [ file "foo.png" "an image" ]
+              ]
+          ]
+      ]
+    and expected =
+      [ dir
+          "."
+          [ file "hello.txt" "Replaced"
+          ; dir
+              "post"
+              [ file "index.md" "First post"
+              ; file "second.md" "second post"
+              ; dir "images" [ file "foo.png" "an image" ]
+              ]
+          ]
+      ]
+    in
+    let computed = write_file fs "./hello.txt" "Replaced" in
+    check fs_testable "should equal" expected computed)
+;;
+
+let test_update_elt_2 =
+  let open Alcotest in
+  test_case "Some test using update_elt" `Quick (fun () ->
+    let fs =
+      [ dir
+          "."
+          [ file "hello.txt" "Hello, World!"
+          ; dir
+              "post"
+              [ file "index.md" "First post"
+              ; file "second.md" "second post"
+              ; dir "images" [ file "foo.png" "an image" ]
+              ]
+          ]
+      ]
+    and expected =
+      [ dir
+          "."
+          [ file "hello.txt" "Hello, World!"
+          ; dir
+              "post"
+              [ file "index.md" "First post"
+              ; file "second.md" "Replaced"
+              ; file "third.md" "Created"
+              ; dir "images" [ file "foo.png" "an image" ]
+              ]
+          ]
+      ]
+    in
+    let computed =
+      fs
+      |> pipe_write_file "./post/second.md" "Replaced"
+      |> pipe_write_file "./post/third.md" "Created"
+    in
+    check fs_testable "should equal" expected computed)
+;;
+
+let test_update_elt_3 =
+  let open Alcotest in
+  test_case "Some test using update_elt" `Quick (fun () ->
+    let fs =
+      [ dir
+          "."
+          [ file "hello.txt" "Hello, World!"
+          ; dir
+              "post"
+              [ file "index.md" "First post"
+              ; file "second.md" "second post"
+              ; dir "images" [ file "foo.png" "an image" ]
+              ]
+          ]
+      ]
+    and expected =
+      [ dir
+          "."
+          [ file "hello.txt" "Hello, World!"
+          ; dir
+              "post"
+              [ file "index.md" "First post"
+              ; file "second.md" "Replaced"
+              ; file "third.md" "Created"
+              ; dir
+                  "images"
+                  [ file "foo.png" "an image"
+                  ; dir "avatar" [ file "xvw.png" "xvw" ]
+                  ]
+              ]
+          ]
+      ; dir "root" [ file "bla.ml" "print_endline" ]
+      ]
+    in
+    let computed =
+      fs
+      |> pipe_write_file "./post/second.md" "Replaced"
+      |> pipe_write_file "./post/third.md" "Created"
+      |> pipe_write_file "root/bla.ml" "print_endline"
+      |> pipe_write_file "./post/images/avatar/xvw.png" "xvw"
+    in
+    check fs_testable "should equal" expected computed)
+;;
+
+let test_cases =
+  ( "Fake_file_system"
+  , [ test_get_elt; test_update_elt_1; test_update_elt_2; test_update_elt_3 ] )
+;;
